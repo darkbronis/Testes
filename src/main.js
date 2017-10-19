2eef10b7903646861b4eba13b5eb9f4f221f2dd9
darkbronis <32820037+darkbronis@users.noreply.github.com>
2017-10-20 03:00:31 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ class LINE extends Command {
 
 
     get myBot() {
-        const bot = ['u3b257ce1497b8d24ec3708ba3ed79d46','u236b88bf1eac2b90e848a6198152e647','u763977dab29cbd6fa0cbfa9f159b768b'];
+        const bot = ['ufdb348d53532a57228f045ecfaa00f8d','ue5060e54a4ed380dcafd0a2213592ad0'];
         return bot; 
     }
 
